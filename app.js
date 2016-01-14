20a3b129ca2a125e8e24b1b18a85966c2369bb57
Dennis Bailey <bailey.dennis@gmail.com>
2016-01-14 15:10:44 -0700

    I did it! Used stock colors for ease

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
 // Write your code in this file
+console.log("This is a test of the emergency broadcast system");
+
+var nav = document.getElementsByTagName('nav')[0];
+  nav.style.backgroundColor = "black";
+  nav.style.color = "white";
+
+var h1 = document.getElementsByTagName('h1')[0];
+  h1.style.textAlign = 'center';
+  h1.style.margin = 0;
+  
+var div0 = document.getElementsByClassName('square')[0];
+var div1 = document.getElementsByClassName('square')[1];
+var div2 = document.getElementsByClassName('square')[2];
+
+div0.style.backgroundColor = "red";
+div1.style.backgroundColor = "orange";
+div2.style.backgroundColor = "yellow";
+
+div0.style.height = "400px";
+div1.style.height = "400px";
+div2.style.height = "400px";
+
+div0.style.width = "33%";
+div1.style.width = "33%";
+div2.style.width = "33%";
+
+div0.style.float = "left";
+div1.style.float = "left";
+div2.style.float = "left";
+
+
+var footer = document.getElementsByTagName('footer')[0];
+  footer.style.backgroundColor = "grey";
+
+var h3 = document.getElementsByTagName('h3')[0];
+  h3.style.textAlign = 'center';
+  h3.style.margin = 0;
